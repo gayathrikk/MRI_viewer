36df8c0af40d60696f2f805b01b88825f5b8b5d1
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-03-18 09:33:34 +0530

    ActionsTest.java

--- src/test/java/Dataportal/MRI_Viewer/ActionsTest.java
+++ src/test/java/Dataportal/MRI_Viewer/ActionsTest.java
@@ -38,7 +38,7 @@ public class ActionsTest {
     @BeforeTest
     public void setup() throws MalformedURLException {
         DesiredCapabilities dc = DesiredCapabilities.chrome();
-        URL url = new URL("http://172.20.23.7:5555/wd/hub");
+        URL url = new URL("http://172.12.20.99:4444/wd/hub");
         driver = new RemoteWebDriver(url, dc);
         wait = new WebDriverWait(driver, 10); // Adjust the timeout as needed
     }
